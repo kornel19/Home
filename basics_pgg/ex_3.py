9e058e93c1718a446d4079063ec19189dc58b4e9
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-03 21:02:30 +0200

    ex 3

--- basics_pgg/ex_3.py
+++ basics_pgg/ex_3.py
@@ -9,3 +9,28 @@ Weight: 2.5
 Total price: 25.0
 """
 
+price = float(input('Provide price per kg: '))
+weight = float(input('Provide weight: '))
+print(type(price), type(weight))
+total_price = price * weight
+
+# 1 method
+print('Price per kg: ' + str(price))
+print('Weight: ' + str(weight))
+print('Total price: ' + str(total_price))
+
+# 2 method
+print('Price per kg:', price)
+print('Weight:', weight)
+print('Total price:', total_price)
+
+# 3 method
+# f-strings, formatted strings
+print(f'Price per kg: {price}')
+print(f'Weight: {weight}')
+
+# format specifiers
+# https://www.python.org/dev/peps/pep-0498/#format-specifiers
+# https://docs.python.org/3.9/library/string.html#formatspec
+print(f'Total price: {total_price:_<10.2f}')
+
