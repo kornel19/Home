553dc952bc59d1a2ddc5d705c028380552c89ff8
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-03 20:20:08 +0200

    ex 2

--- basics_pgg/ex_1.py
+++ basics_pgg/ex_1.py
@@ -4,6 +4,10 @@ the trapezoid field with base lengths 3 and 9 and height 6.5.
 formula = 1/2*(base1+base2)*height
 """
 
+# PEP - Python Enhancements Proposal
+# PEP8 - Style guide https://www.python.org/dev/peps/pep-0008
+# Naming convention https://realpython.com/python-pep8/#naming-conventions
+
 short_base = 3
 long_base = 9
 height = 6.5
