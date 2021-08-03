b55c45c51eb75842421ea2e506038a4fee32f393
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-03 20:02:20 +0200

    ex 1

---
+++ basics_pgg/ex_1.py
@@ -0,0 +1,5 @@
+"""
+Using variable assignment, write a program to calculate
+the trapezoid field with base lengths 3 and 9 and height 6.5.
+"""
+
--- basics_pgg/intro.py
+++ basics_pgg/intro.py
@@ -5,3 +5,47 @@
 print("Hello world!")
 print(2 + 2)
 
+# Data types
+
+# string - str
+print("Hello world!")
+print('Hello world!')
+
+# print(Hello world) - this will not work, will cause SyntaxError
+
+# integers - int
+print(10)
+print(-5)
+
+# float
+print(3.14)
+print(-10.5)
+print(3.0)
+
+# bool
+print(True)
+print(False)
+
+# None -> value that represent the lack of value
+# empty value
+# null -> in other programming languages
+
+print(type(10))  # int
+print(type(10.0))  # float
+print(type("10"))  # str
+
+# Operators
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)
+print(10 ** 3)
+print("I see " + "the cat")
+print("-" * 10)
+
+# variables
+first_name = "Piotr"
+print(first_name)
+first_name = 'Tom'
+print(first_name)
