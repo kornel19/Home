c1f6a78d1bd9f8c9fa303b9af876d1406d6bafb1
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-05 21:25:39 +0200

    ex 10

---
+++ basics_pgg/ex_10.py
@@ -0,0 +1,11 @@
+"""
+Write a program that, based on the player’s position (x, y) on the board in the range from 0 to 100,
+displays its approximate location (center, upper right corner, upper edge, ...)
+or information about the position outside the board. Take the value 10 as an edge margin.
+
+Sample program output:
+Enter player's X position: 95
+Enter player's Y position: 95
+The player is in the upper right corner.
+"""
+
