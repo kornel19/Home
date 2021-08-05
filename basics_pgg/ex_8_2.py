dafabadce67e491ac494cf3b8237ae8cc3874ff7
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-05 20:19:38 +0200

    ex 8

--- basics_pgg/ex_8_2.py
+++ basics_pgg/ex_8_2.py
@@ -7,8 +7,12 @@ Enter year of birth: 1980
 You are of legal age!
 """
 
+import datetime
+
 year_of_birth = int(input('Provide your year of birth: '))
-age = 2021 - year_of_birth
+current_year = datetime.datetime.now().year
+print(current_year)
+age = current_year - year_of_birth
 
 if age >= 18:
     print('You are of a legal age. Grab a beer!')
@@ -16,12 +20,3 @@ else:
     print('You are a minor. Some sweets for ya!')
 
 print('Thank you for cooperation. Good bye!')  # watch out for indention!!!
-
-# in Java we would write
-# if (age >= 18) {
-#     print('You are of a legal age. Grab a beer!');
-# } else {
-#     print('You are a minor. Some sweets for ya!');
-# }
-#
-# print('Thank you for cooperation. Good bye!')  # watch out for indention!!!
