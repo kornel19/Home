85ab322f58f136193f8a30ae88b76611b29c44b8
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-10 19:01:32 +0200

    ex 12

--- basics_pgg/ex_12.py
+++ basics_pgg/ex_12.py
@@ -3,3 +3,16 @@ Write a program that calculates the average temperature
 for a given week based on temperatures entered by the user.
 """
 
+day_number = 1
+temperature_sum = 0
+
+while day_number <= 7:
+    temperature_sum += int(input(f'Provide temp for day {day_number}: '))
+    print(temperature_sum)
+    day_number += 1
+
+# calculate the average temperature
+average_temperature = temperature_sum / 7
+
+# print the results
+print(f'Average temperature for this week is {average_temperature:.2f}')
