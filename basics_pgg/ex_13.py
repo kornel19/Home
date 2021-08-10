85bb287d30df2f82862e1effc0fca5fb3e266009
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2021-08-10 19:41:14 +0200

    ex 13

--- basics_pgg/ex_13.py
+++ basics_pgg/ex_13.py
@@ -12,3 +12,34 @@ How we can do that?
         - check the existing value in minimum/maximum variable - if None, assign a value from the user
         - check the provided number if greater than maximum or lower than minimum and then properly assign new value
 """
+
+minimum = None
+maximum = None
+
+while True:
+    input_data = input('Provide a number or STOP: ')
+
+    # if input_data equals STOP break the loop
+    if input_data == 'STOP':
+        break  # breaks the loop, no code from the loop will be executed after break is fired
+
+    # convert input_data to int
+    try:
+        number = int(input_data)
+    except ValueError:
+        print('The number you have provided is invalid')
+        continue
+
+    # if minimum is None or provided number is less than minimum assign this value to minimum
+    if minimum is None or number < minimum:
+        minimum = number
+
+    # do the same with maximum
+    if maximum is None or number > maximum:
+        maximum = number
+
+# after the loop present results
+if minimum is None or maximum is None:
+    print('You didn\'t provided any numbers.')
+else:
+    print(f'Provided minimum = {minimum}, maximum = {maximum}')
